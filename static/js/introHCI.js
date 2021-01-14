360ef427a95bed7228046a11c93c543b662b02ee
Theresa Sobocinski <tsobocin@ucsd.edu>
2021-01-14 11:24:41 -0800

    added Javascript git push git clear git reset

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -3,7 +3,13 @@ $(document).ready(function() {
 });
 
 function initializePage() {
+  $("a.thumbnail").click(projectClick);
     $('#testjs').click(function(e) {
             $('.jumbotron h1').text("Javascript is connected");
             });
+}
+
+function projectClick(e){
+  e.preventDefault();
+  $(this).css("background-color", "#7fff00");
 }
